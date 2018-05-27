a8a3ad1a0ea104e015bb296c049c011d50097361
Leonardo <leosilvadev@gmail.com>
2018-05-27 23:00:00 +0200

    adding comment to our client

--- src/main/java/com/github/leosilvadev/nonblockingjava/nonblocking/ClientNonBlocking.java
+++ src/main/java/com/github/leosilvadev/nonblockingjava/nonblocking/ClientNonBlocking.java
@@ -42,6 +42,7 @@ public class ClientNonBlocking {
     return this;
   }
 
+  // Keep running still we have some Socket Channel that is not complete (counter has this info)
   private static void readNonBlocking(final Selector selector, final CountDownLatch counter, final Consumer<Context> callback) throws IOException {
     while(counter.getCount() > 0) {
       selector.select();
@@ -54,12 +55,20 @@ public class ClientNonBlocking {
         final SocketChannel channel = (SocketChannel) key.channel();
         channel.configureBlocking(false);
 
+        // As soon as we have a connectable channel, we should finish to connect to server
         if (key.isConnectable()) {
           channel.finishConnect();
 
+          // If the channel is ready to execute READ operation, then we start to read data from that with a ByteBuffer
+          // We are using a Context, that represents the context in which the channel is executing, this contains an id, the beginning time and also the partial response that was read
         } else if (key.isReadable()) {
           final Context context = (Context) key.attachment();
+
+          // Since it is non-blocking and don't always read a response in a shot, we can have partial response (partial json)
+          // so that we are wrapping this in a class that knows when the response is complete
           final PartialResponse partialResponse = readData(channel, context.getPartialResponse());
+
+          // When the response is complete we remove the channel from our selector
           if (partialResponse.isComplete()) {
             callback.accept(context.copy(partialResponse));
             key.cancel();
@@ -78,18 +87,23 @@ public class ClientNonBlocking {
   }
 
   private static PartialResponse readData(final SocketChannel channel, final PartialResponse partialResponse) throws IOException {
-    final ByteBuffer buffer = ByteBuffer.allocate(4096);
+    final ByteBuffer buffer = ByteBuffer.allocate(256);
     channel.read(buffer);
 
+    // Partial reading forces us to be ready for that, receiving data like: <BEGIN>\n{"id:
     final String[] messages = new String(buffer.array()).trim().split("\n");
     buffer.clear();
 
     for (String message : messages) {
+      // Beginning of the message, does nothing
       if (message.equals(IOUtil.BEGIN_MSG)) {
+
+        // End of message we close the partial response and return it as completed
       } else if (message.equals(IOUtil.END_MSG)) {
         return new PartialResponse(partialResponse.getContent(), true);
 
       } else {
+        // Not end of message we just append the new partial-content to the existent one
         partialResponse.appendContent(message);
 
       }
@@ -102,12 +116,18 @@ public class ClientNonBlocking {
     final Selector selector = Selector.open();
     final CountDownLatch counter = new CountDownLatch(clients);
 
+    // Create one new client and connect then to the server
+    // by connecting then, it means: connect and register the channel in the selector
+    // so in readNonBlocking method we will monitor the channel
     for (Integer client = 0 ; client < clients ; client++) {
       new ClientNonBlocking(new InetSocketAddress(8080), selector).connect();
     }
 
     final AtomicInteger id = new AtomicInteger(0);
 
+    // We start to monitor all the channels for all the clients we have
+    // as soon as the channel reads all it needs, the closure callback will be executed
+    // Pls check the read-non-blocking implementation!
     readNonBlocking(selector, counter, context -> {
       final long endOfExecution = System.currentTimeMillis();
       final long executionTime = endOfExecution - context.getBeganAt();
